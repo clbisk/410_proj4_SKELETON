05199c899845c79e0c96d7db5fa0fd72a1320f2a
Your Name <you@example.com>
2020-04-16 13:35:29 -0400

    finished box_and_bake

--- baker/Baker.cpp
+++ baker/Baker.cpp
@@ -1,8 +1,12 @@
 #include <mutex>
+#include <string>
 
 #include "../includes/baker.h"
+#include "../includes/externs.h"
 using namespace std;
 
+//ID is just a number used to identify this particular baker 
+//(used with PRINT statements)
 Baker::Baker(int id):id(id)
 {
 }
@@ -11,10 +15,30 @@ Baker::~Baker()
 {
 }
 
+//bake, box and append to anOrder.boxes vector
+//if order has 13 donuts there should be 2 boxes
+//1 with 12 donuts, 1 with 1 donut
 void Baker::bake_and_box(ORDER &anOrder) {
+	for (int i = 0; i < anOrder.number_donuts; i++) {
+		if (i % 12 = 0) {
+			Box newBox = Box();
+			anOrder.boxes.push_back(newBox);
+		}
+	}
 	
 }
 
+//as long as there are orders in order_in_Q then
+//for each order:
+//	create box(es) filled with number of donuts in the order
+//  then place finished order on order_outvector
+//  if waiter is finished (b_WaiterIsFinished) then
+//  finish up remaining orders in order_in_Q and exit
+//
+//You will use cv_order_inQ to be notified by waiter 
+//when either order_in_Q.size() > 0 or b_WaiterIsFinished == true
+//hint: wait for something to be in order_in_Q or b_WaiterIsFinished == true
 void Baker::beBaker() {
+	
 	
 }
