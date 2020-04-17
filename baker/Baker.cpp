7dd38608f393f111c549d5e18229331402602dbf
Your Name <you@example.com>
2020-04-17 13:30:40 -0400

    did some work on beBaker

--- baker/Baker.cpp
+++ baker/Baker.cpp
@@ -15,9 +15,10 @@ Baker::~Baker() { }
 //if order has 13 donuts there should be 2 boxes
 //1 with 12 donuts, 1 with 1 donut
 void Baker::bake_and_box(ORDER &anOrder) {
-	//bake the donuts: create a Donut object for number_donuts (I recommend storing them in a vector for the next step)
+	//bake the donuts: create a Donut object for number_donuts 
+	//(I recommend storing them in a vector for the next step)
 	//...
-
+	DONUT donut = DONUT();
 	//box the donuts and add them to the order
 	for (int i = 0; i < anOrder.number_donuts; i += 12) {
 		Box newBox = Box();
@@ -40,25 +41,23 @@ void Baker::bake_and_box(ORDER &anOrder) {
 //when either order_in_Q.size() > 0 or b_WaiterIsFinished == true
 //hint: wait for something to be in order_in_Q or b_WaiterIsFinished == true
 void Baker::beBaker() {
-	if (!b_WaiterIsFinished) {
-		while (!order_in_Q.empty()) {	//as long as there are orders in order_in_Q
-			//make sure order_in_Q is not locked (wait on cv_order_inQ)
-			//...
+	while (!order_in_Q.empty() && !b_WaiterIsFinished) {	//as long as there are orders in order_in_Q
+		//make sure order_in_Q is not locked (wait on cv_order_inQ)
+		//...
+		cv_order_inQ.wait(mutex_order_inQ);
 
-			unique_lock<mutex> lck(mutex_order_outQ);
-			ORDER currOrder = order_in_Q.front();
+		unique_lock<mutex> lck(mutex_order_outQ);
+		ORDER currOrder = order_in_Q.front();
 
-			//bake Donuts and fill them into Boxes
-			bake_and_box(currOrder);
+		//bake Donuts and fill them into Boxes
+		bake_and_box(currOrder);
 
-			//next add the order to the outVector
-			order_out_Vector.push_back(currOrder);
+		//next add the order to the outVector
+		order_out_Vector.push_back(currOrder);
 
-			//remove the first ORDER in the in_Q
-			order_in_Q.pop();
-		}
-
-		//!!! what happens when the baker empties the order_in_Q but the waiter is not done yet? !!!
-		//see 410_Producer_many_consumers on the course website
+		//remove the first ORDER in the in_Q
+		order_in_Q.pop();
 	}
+	//!!! what happens when the baker empties the order_in_Q but the waiter is not done yet? !!!
+	//see 410_Producer_many_consumers on the course website
 }
