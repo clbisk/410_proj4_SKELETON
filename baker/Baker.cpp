e4740418ba85855c34b9ce4bade7ff85dae64172
Cecilia Bisk <cecilia.bisk.17@cnu.edu>
2020-04-16 19:32:33 -0700

    added comments to Baker

--- baker/Baker.cpp
+++ baker/Baker.cpp
@@ -7,26 +7,26 @@ using namespace std;
 
 //ID is just a number used to identify this particular baker 
 //(used with PRINT statements)
-Baker::Baker(int id):id(id)
-{
-}
+Baker::Baker(int id):id(id) { }
 
-Baker::~Baker()
-{
-}
+Baker::~Baker() { }
 
 //bake, box and append to anOrder.boxes vector
 //if order has 13 donuts there should be 2 boxes
 //1 with 12 donuts, 1 with 1 donut
 void Baker::bake_and_box(ORDER &anOrder) {
-	for (int i = 0; i < anOrder.number_donuts; i++) {
-		if (i % 12 == 0) {
-			Box newBox = Box();
-			anOrder.boxes.push_back(newBox);
-		}
-		Box currBox = anOrder.boxes.end();
+	//bake the donuts: create a Donut object for number_donuts (I recommend storing them in a vector for the next step)
+	//...
+
+	//box the donuts and add them to the order
+	for (int i = 0; i < anOrder.number_donuts; i += 12) {
+		Box newBox = Box();
+
+		//add 12 Donuts, or until you are out of baked Donuts
+		//...
+
+		anOrder.boxes.push_back(newBox);
 	}
-	
 }
 
 //as long as there are orders in order_in_Q then
@@ -40,33 +40,25 @@ void Baker::bake_and_box(ORDER &anOrder) {
 //when either order_in_Q.size() > 0 or b_WaiterIsFinished == true
 //hint: wait for something to be in order_in_Q or b_WaiterIsFinished == true
 void Baker::beBaker() {
-	if (b_WaiterIsFinished) {
+	if (!b_WaiterIsFinished) {
 		while (!order_in_Q.empty()) {	//as long as there are orders in order_in_Q
+			//make sure order_in_Q is not locked (wait on cv_order_inQ)
+			//...
+
 			unique_lock<mutex> lck(mutex_order_outQ);
 			ORDER currOrder = order_in_Q.front();
-			//create box(es)
-			bake_and_box(currOrder);
-			//filled with number of donuts in the order
-			Box currBox = currOrder.boxes.front();
-			for (int i = 0; i < int(currOrder.number_donuts); i++) {
-				//if the below statement returns false the box is full
-				//and you need to change curr box
-				if (!currBox.addDonut(currBox.donuts.front()) {	//having trouble accessing donuts, it is private
-					//change curr box
-					//	currBox = boxes.get_next();
-				}
-				//this is where we will add the donuts to the box
-				//.......
 
-			}
+			//bake Donuts and fill them into Boxes
+			bake_and_box(currOrder);
 
 			//next add the order to the outVector
-			//	order_out_Vector.push_back(currOrder); ?? this isn't working
+			order_out_Vector.push_back(currOrder);
 
 			//remove the first ORDER in the in_Q
 			order_in_Q.pop();
-			
 		}
+
+		//!!! what happens when the baker empties the order_in_Q but the waiter is not done yet? !!!
+		//see 410_Producer_many_consumers on the course website
 	}
-	
 }
